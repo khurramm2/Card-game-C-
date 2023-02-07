f422ee319f275006d333a6b9a3d20bac4e45385d
khurramm2 <khurramm2@uni.coventry.ac.uk>
2023-02-07 09:28:22 +0000

    Update enemies.cpp

---
+++ enemies.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class enemies{
+private:
+
+protected:
+    string enemy, special;
+    int health, attack, block;
+public:
+    int player_health = 100;
+
+    void enemy_general(){
+
+    }
+
+
+    enemies(string enemy, special)
+};
