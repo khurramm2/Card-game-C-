a88fb784a987c40caedab2742007f83e27e6243d
khurramm2 <khurramm2@uni.coventry.ac.uk>
2023-03-16 12:02:24 +0000

    b

--- main.cpp
+++ main.cpp
@@ -9,61 +9,42 @@
 #include <vector>
 using namespace std;
 
-//player_health = 100;
-
 int turns(){
-    //cout << "type something in ";
     if (getch() == 13) {
         turn_num++;
         //cout << turn_num << endl;
         return turn_num;
     
     }
-    else if(getch()==99){
-        //displayCards();
-        cout<< "hey";
-    }
-
-    else if(getch()==100){
-    //;
-    }
-
+    // else if(getch()==99){
+    //     displayCards();
+    // }
     else{
         cout << "Please type enter to end your turn";
     }
     return turn_num;
-
 }
 
-//note in the child classes I cannot access the constructor variables.
 
-vector<string> hand = {Attack.Card_Name, Attack.Card_Name, Attack.Card_Name, Block.Card_Name, Block.Card_Name};
+
 int main() {
     // cout << "Welcome to dungeon fighter" << endl;
-    // cout << "You have encountered an enemy \n";
+    cout << "You have encountered an enemy \n";
+    while (true){
+        turns();
 
-    // while (true){
-    //     turns();
+        if (turn_num % 2 == 0){
+            slime.enemy_attack();
+            cout << "Your health is... " << player_health << endl;
+        }
 
-    //     if (turn_num % 2 == 0){
-    //         slime.enemy_attack();
-            
-    //         cout << "Your health is... " << player_health << endl;
-    //     }
-
-    //     else {
-    //         "You did 5 damage";
-    //         slime.Health -= 5;
-    //         cout << slime.Health << endl;
-    //     }
+        else {
+            "You did 5 damage";
+            slime.Health -= 5;
+            cout << slime.Health << endl;
+        }
         
-    // }
-    
-    
-    //cout << player_health;
-    Cards_Curse nullcurse = {"curse", "does nothing", 0, 0};
-    cout << nullcurse.Card_Name;
-
+    }
 }
 
 
